69f6d9c3265b72443578b13a6b81dcc901099da3
Craig <craig.williams@acutesales.co.uk>
2014-06-04 21:11:28 +0100

    initial commit

---
+++ N2R/Scripts/markdown/demo.js
@@ -0,0 +1,10 @@
+var opts = {
+  basePath: '',
+  theme: {
+    base: 'Scripts/markdown/epiceditor.css',
+    preview: 'Scripts/markdown/bartik.css',
+    editor: 'Scripts/markdown/epic-light.css'
+  }
+}
+
+var editor = new EpicEditor(opts).load();
---
+++ N2R/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(N2R.Startup))]
+namespace N2R
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
